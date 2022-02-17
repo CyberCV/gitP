e527b8eea27d89783589a6cfe2ade5ae15be7ae8
CyberCV <99675601+CyberCV@users.noreply.github.com>
2022-02-16 19:28:51 -0500

    first commit

---
+++ src/gitclass03/gitTest.java
@@ -0,0 +1,10 @@
+package gitclass03;
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("water si colorless");
+	}
+
+}
