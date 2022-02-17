2f23febd981f52197fc1f6bc6d0412437d508d1f
CyberCV <99675601+CyberCV@users.noreply.github.com>
2022-02-16 19:39:05 -0500

    second commit

--- src/gitclass03/gitTest.java
+++ src/gitclass03/gitTest.java
@@ -4,7 +4,8 @@ public class gitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("water si colorless");
+		System.out.print("water is colorless");
+		System.out.print(" water is blue");
 	}
 
 }
